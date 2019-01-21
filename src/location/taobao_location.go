c8d41fa46ba8a6ff0f1790114849a1e30c14fd2b
hiscaler <hiscaler@gmail.com>
2019-01-21 17:25:26 +0800

    完善 TaobaoLocation

--- src/location/taobao_location.go
+++ src/location/taobao_location.go
@@ -1,10 +1,12 @@
 package location
 
+// ip.taobao.com
 import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type TaoBaoLocation struct {
@@ -13,9 +15,15 @@ type TaoBaoLocation struct {
 
 func (t *TaoBaoLocation) Find() (Location, error) {
 	t.Name = "TaoBao"
-	resp, err := http.Get("http://ip.taobao.com/service/getIpInfo.php?ip=" + t.Ip)
-	if err == nil {
-		defer resp.Body.Close()
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://ip.taobao.com/service/getIpInfo.php?ip="+t.Ip, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+	req.Header.Set("Host", "ip.taobao.com")
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Pragma", "no-cache")
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err == nil && resp.StatusCode == 200 {
 		if body, err := ioutil.ReadAll(resp.Body); err == nil {
 			respJson := struct {
 				Code int
@@ -36,19 +44,27 @@ func (t *TaoBaoLocation) Find() (Location, error) {
 				}
 			}{}
 			if err := json.Unmarshal(body, &respJson); err == nil {
+				parseValue := func(s string) string {
+					s = strings.ToLower(s)
+					if s == "xx" {
+						return ""
+					} else {
+						return s
+					}
+				}
 				if respJson.Code == 0 {
-					t.CountryId = respJson.Data.CountryId
-					t.CountryName = respJson.Data.Country
-					t.AreaId = respJson.Data.AreaId
-					t.AreaName = respJson.Data.Area
-					t.ProvinceId = respJson.Data.RegionId
-					t.ProvinceName = respJson.Data.Region
-					t.CityId = respJson.Data.CityId
-					t.CityName = respJson.Data.City
-					t.RegionId = respJson.Data.CountyId
-					t.RegionName = respJson.Data.County
-					t.IspId = respJson.Data.IspId
-					t.IspName = respJson.Data.Isp
+					t.CountryId = parseValue(respJson.Data.CountryId)
+					t.CountryName = parseValue(respJson.Data.Country)
+					t.AreaId = parseValue(respJson.Data.AreaId)
+					t.AreaName = parseValue(respJson.Data.Area)
+					t.ProvinceId = parseValue(respJson.Data.RegionId)
+					t.ProvinceName = parseValue(respJson.Data.Region)
+					t.CityId = parseValue(respJson.Data.CityId)
+					t.CityName = parseValue(respJson.Data.City)
+					t.RegionId = parseValue(respJson.Data.CountyId)
+					t.RegionName = parseValue(respJson.Data.County)
+					t.IspId = parseValue(respJson.Data.IspId)
+					t.IspName = parseValue(respJson.Data.Isp)
 
 					return t.Location, nil
 				} else {
@@ -60,6 +76,7 @@ func (t *TaoBaoLocation) Find() (Location, error) {
 		} else {
 			return t.Location, err
 		}
+
 	} else {
 		return t.Location, err
 	}
